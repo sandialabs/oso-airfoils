eab366df41d84f1aff4a43e48fceb2d41284e9ee
Cody Karcher <ckarcher@umich.edu>
2025-08-27 13:06:16 -0700

    adding rfoil data

---
+++ postprocessing/cases/cases_61_to_70/case_68/run_all.sh
@@ -0,0 +1,42 @@
+# cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t15_l15_r124_k16_n400__2025_08_14_07-39
+# mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t18_l15_r123_k16_n400__2025_08_15_00-53
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l10_r122_k16_n400__2025_08_15_01-15
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l11_r122_k16_n400__2025_08_15_01-15
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l12_r122_k16_n400__2025_08_15_02-02
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l13_r122_k16_n400__2025_08_15_01-18
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l14_r122_k16_n400__2025_08_15_01-29
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r115_k16_n400__2025_08_15_01-44
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r116_k16_n400__2025_08_15_01-27
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r117_k16_n400__2025_08_15_02-04
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r118_k16_n400__2025_08_15_02-05
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r119_k16_n400__2025_08_15_02-03
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r120_k16_n400__2025_08_15_01-23
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r121_k16_n400__2025_08_15_01-23
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t21_l15_r122_k16_n400__2025_08_15_01-21
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t24_l14_r118_k16_n376__2025_08_15_00-57
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t24_l14_r118_k16_n400__2025_08_15_00-50
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t27_l13_r111_k16_n400__2025_08_15_00-55
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t30_l12_r98_k16_n400__2025_08_15_01-12
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t33_l12_r90_k16_n400__2025_08_15_01-28
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
+cd /Users/codykarcher/Desktop/oso-airfoils/postprocessing/cases/cases_61_to_70/case_68/c68_t36_l12_r78_k16_n400__2025_08_15_01-56
+mpirun -n 8 python -m mpi4py postprocess.py 0 1
