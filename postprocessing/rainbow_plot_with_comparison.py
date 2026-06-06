6c5244aed28c73ec590df30efec276e31ddfa800
Cody Karcher <ckarcher@umich.edu>
2026-06-06 04:42:53 -0700

    adding case 115 data (t18 runs) and postprocessing updates

--- postprocessing/rainbow_plot_with_comparison.py
+++ postprocessing/rainbow_plot_with_comparison.py
@@ -9,13 +9,29 @@ path_to_here = pathlib.Path(__file__).parent.resolve()
 path_to_oso = path_to_here.parent
 path_to_datfiles = path_to_oso / 'historical_airfoils/mhkf1/'
 
-# path_to_data = path_to_here / "cases/cases_111_to_120/case_115/c115_t18_k16_n752_l13_e15__2026_05_19_23-59-1193/population_c115_t18_k16_n752_l13_e15_g065.json"
-path_to_data = "/Users/codykarcher/Dropbox/research/workstation/wt_airfoil_case_115/c115_t21_k16_n752_l13_e15__2026_05_20_08-06-4000/population_c115_t21_k16_n752_l13_e15_g217.json"
+# # path_to_data = path_to_here / "cases/cases_111_to_120/case_115/c115_t18_k16_n752_l13_e15__2026_05_19_23-59-1193/population_c115_t18_k16_n752_l13_e15_g065.json"
+path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t18_k16_n752_l13_e15__2026_06_04_04-56-3876/population_c115_t18_k16_n752_l13_e15_g500.json"
 comp = {"mhkf1-180":str(path_to_datfiles / 'mhkf1-180.dat')}
 cor = {"mhkf1-180":'k'}
 rainbow_plot(path_to_data, comparison_airfoil=comp, color_override=cor)#, tools = ['xfoil'])
 
-# path_to_data = path_to_here / "cases/cases_111_to_120/case_114/c114_t24_k16_n752_l13_e15__2026_05_14_09-51-5547/population_c114_t24_k16_n752_l13_e15_g500.json"
+# path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t18_k16_n752_l13_e15__2026_05_21_00-51-0784/population_c115_t18_k16_n752_l13_e15_g500.json"
+# comp = {"mhkf1-180":str(path_to_datfiles / 'mhkf1-180.dat')}
+# cor = {"mhkf1-180":'k'}
+# rainbow_plot(path_to_data, comparison_airfoil=comp, color_override=cor)#, tools = ['xfoil'])
+
+# path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t21_k16_n752_l13_e15__2026_05_20_08-06-4000/population_c115_t21_k16_n752_l13_e15_g500.json"
+# rainbow_plot(path_to_data, comparison_airfoil={}, color_override={})#, tools = ['xfoil'])
+
+# path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t21_k16_n752_l13_e15__2026_05_21_08-38-0187/population_c115_t21_k16_n752_l13_e15_g500.json"
+# rainbow_plot(path_to_data, comparison_airfoil={}, color_override={})#, tools = ['xfoil'])
+
+# # path_to_data = path_to_here / "cases/cases_111_to_120/case_114/c114_t24_k16_n752_l13_e15__2026_05_14_09-51-5547/population_c114_t24_k16_n752_l13_e15_g500.json"
+# path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t24_k16_n752_l13_e15__2026_05_20_17-04-3892/population_c115_t24_k16_n752_l13_e15_g500.json"
 # comp = {"mhkf1-240":str(path_to_datfiles / 'mhkf1-240.dat')}
 # cor = {"mhkf1-240":'k'}
 # rainbow_plot(path_to_data, comparison_airfoil=comp, color_override=cor)#, tools = ['xfoil'])
+
+
+# path_to_data = "/Users/codykarcher/Dropbox/research/oso-airfoils/postprocessing/cases/cases_111_to_120/case_115/c115_t33_k16_n752_l12_e15__2026_05_22_15-47-1615/population_c115_t33_k16_n752_l12_e15_g500.json"
+# rainbow_plot(path_to_data, comparison_airfoil={}, color_override={})#, tools = ['xfoil'])
